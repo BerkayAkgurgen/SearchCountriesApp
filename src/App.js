f460d263ba1c7c1885014d1fcb8e5526c7d17e30
Berkay Akgürgen <akgurgennberkay@gmail.com>
2021-05-14 12:11:15 +0300

    Navbar component eklendi.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import Navbar from "./components/header/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useParams,
+  useHistory,
+} from "react-router-dom";
 
 export function App() {
-  return <div>
-  </div>;
+  return (
+    <div className="app">
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  );
 }
