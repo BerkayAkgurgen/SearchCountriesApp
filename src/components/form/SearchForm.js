e4de534816aa0801b3bda552e88d76f37920ee68
Berkay Akgürgen <akgurgennberkay@gmail.com>
2021-05-16 05:14:34 +0300

    Form dosyası yeniden oluşturuldu.

---
+++ src/components/form/SearchForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+
+const SearchForm = () => {
+  return (
+    <div className="section__search search">
+      <form className="search__form">
+        <input
+          tabIndex="1"
+          type="text"
+          className="search__form-input"
+          placeholder="e.g Turkey, Germany..."
+        />
+        <button className="search__form-button">
+          <BiSearchAlt />
+        </button>
+      </form>
+      <div className="search__filter filter">
+        <select tabIndex="2" name="filter-search" className="filter-search">
+          <option hidden value="Searched By:">
+            Search by:
+          </option>
+          <option value="name">Name</option>
+          <option value="language">Language</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
