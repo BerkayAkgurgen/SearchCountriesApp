049cfb710b85201fba1fd2c570916908d70d95ce
Berkay Akgurgen <akgurgennberkay@gmail.com>
2021-05-29 18:21:51 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require("file-loader?name=[name].[ext]!./index.html");
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./styles/App.scss";
-
-const appElement = document.getElementById("app");
-
-ReactDOM.render(<App />, appElement);
